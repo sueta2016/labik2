b7cb1f860ea3743874a746b600ef1173004b7480
olegoofy <martynenkooleg77@gmail.com>
2023-03-18 16:45:04 +0200

    Added implementation and tests

--- implementation.go
+++ implementation.go
@@ -1,9 +1,92 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"strconv"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func isNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
+func prefixToPostfix(prefixExpression string) (string, error) {
+
+	tokens := strings.Fields(prefixExpression)
+
+	stack := []string{}
+
+	
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+		
+		if token == "+" || token == "-" || token == "*" || token == "/" || token == "^" {
+			if len(stack) < 2 {
+				return "", errors.New("insufficient operands for operator")
+			}
+			first_operand := stack[len(stack)-1]
+			second_operand := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			output := fmt.Sprintf("%s %s %s", first_operand, second_operand, token)
+			stack = append(stack, output)
+		} else if isNumeric(token) {
+			stack = append(stack, token)
+		} else {
+			return "", errors.New("Undefined operator, enter only math operations and numeric values")
+		}
+	}
+
+	
+	if len(stack) != 1 {
+		return "", errors.New("error converting expression")
+	}
+
+	
+	postfixExpression := stack[0]
+	return postfixExpression, nil
+}
+
+func main() {
+	
+	prefixExpression1 := "^ 2 3.5"
+	prefixExpression2 := "* + 2 3 4"
+	prefixExpression3 := "- / * 3 4 + 5 6 7"
+	prefixExpression4 := "+ 1 4 + +"
+
+	
+	postfixExpression1, err1 := prefixToPostfix(prefixExpression1)
+	postfixExpression2, err2 := prefixToPostfix(prefixExpression2)
+	postfixExpression3, err3 := prefixToPostfix(prefixExpression3)
+	postfixExpression4, err4 := prefixToPostfix(prefixExpression4)
+
+	
+	fmt.Println("Input expression 1:", prefixExpression1)
+	if err1 != nil {
+		fmt.Println("Error:", err1)
+	} else {
+		fmt.Println("Output expression 1:", postfixExpression1)
+	}
+
+	fmt.Println("Input expression 2:", prefixExpression2)
+	if err2 != nil {
+		fmt.Println("Error:", err2)
+	} else {
+		fmt.Println("Output expression 2:", postfixExpression2)
+	}
+
+	fmt.Println("Input expression 3:", prefixExpression3)
+	if err3 != nil {
+		fmt.Println("Error:", err3)
+	} else {
+		fmt.Println("Output expression 3:", postfixExpression3)
+	}
+
+	fmt.Println("Input expression 4:", prefixExpression4)
+	if err4 != nil {
+		fmt.Println("Error:", err4)
+	} else {
+		fmt.Println("Output expression 4:", postfixExpression4)
+	}
 }
--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIsNumeric(t *testing.T) {
+    assert.True(t, isNumeric("123"))
+    assert.True(t, isNumeric("0.5"))
+    assert.True(t, isNumeric("-10"))
+    assert.False(t, isNumeric("abc"))
+    assert.False(t, isNumeric("123a"))
+}
+
 func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+    testCases := []struct {
+        prefixExpression  string
+        postfixExpression string
+        err         error
+    }{
+        {"+ 2 3.5", "2 3.5 +", nil},
+        {"* + 2 3 4", "2 3 + 4 *", nil},
+        {"- / * 3 4 + 5 6 7", "3 4 * 5 6 + / 7 -", nil},
+        {"+ 1 4 + +", "", errors.New("insufficient operands for operator")},
+        {"+", "", errors.New("insufficient operands for operator")},
+        {"a b c", "", errors.New("Undefined operator, enter only math operations and numeric values")},
+    }
+
+    for _, tc := range testCases {
+        postfixExpression, err := prefixToPostfix(tc.prefixExpression)
+        assert.Equal(t, tc.err, err)
+        assert.Equal(t, tc.postfixExpression, postfixExpression)
+    }
+
 }
 
 func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
-
-	// Output:
-	// 2 2 +
+	res, err := prefixToPostfix("+ 3 4")
+	if err == nil {
+		fmt.Println(res)
+	} else {
+		panic(err)
+	}
 }
